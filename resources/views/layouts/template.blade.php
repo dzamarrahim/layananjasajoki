fa7b8370af564054c806ccda88f64317e6cf56f6
dzamarrahim <dzamarrahim@gmail.com>
2024-06-07 09:59:03 +0700

    Memperbaiki Sidebar

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -77,7 +77,7 @@
             </a>
 
             <!-- Sidebar -->
-            <div class="sidebar" style="min-height: 800px;">
+            <div class="sidebar" style="min-height: 860px;">
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
